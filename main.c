13fb5a7f2f93a4633d9d6ea4a3aa6fc14e50bd8f
이성은 <katrina060208@gmail.com>
2025-10-16 16:13:08 +0900

    실습6-1

--- main.c
+++ main.c
@@ -1,28 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void sub(void); // function declare
+int all_files; // global variable
+
+extern void sub();
 
 int main(void)
 {
-	int i; // local variable
-	
-	for(i=0; i<3; i++)
-		sub();
+	sub();
+	printf("%d\n", all_files);
 	
 	system("PAUSE");
 	return 0;
 }
-
-void sub(void) // funtion definiton
-{
-	int auto_count=0; // automatic allocation
-	static int static_count=0; // static allocation
-	
-	auto_count++;
-	static_count++;
-	
-	printf("auto_count = %d\n", auto_count);
-	printf("static_count = %d\n", static_count);
-}
-
